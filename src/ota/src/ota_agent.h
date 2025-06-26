beb54c346c51364f47816dee24936772914eb7de
tbardy <tbardy@gmail.com>
2025-06-26 09:28:45 -0700

    Implement OTA mechanism (use mender-mcu module)
    Mender is an update system with simple client/server architecture.
    A Mender server instance is required and the TENANT_TOKEN must be paste
    in the prj.conf file.
    
    https://github.com/mendersoftware/mender-mcu

---
+++ src/ota/src/ota_agent.h
@@ -0,0 +1,17 @@
+/**
+ * @file ota_agent.h
+ * @author Theodore Bardy
+ *
+ * @note This file is part of the Zephyr Demo project from Witekio
+ * @brief Agent for managing OTA updates
+ */
+
+#ifndef OTA_AGENT_H
+#define OTA_AGENT_H
+
+/**
+ * @brief Initializes the OTA agent
+ */
+void ota_agent_init(void);
+
+#endif // OTA_AGENT_H
